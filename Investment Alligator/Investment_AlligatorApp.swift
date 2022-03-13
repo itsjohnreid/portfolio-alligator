46f7c0a2b74f9d2d4024e4c0e9c366e1f0ce93f5
Johnathan Reid <itsjohnreid@gmail.com>
2022-03-13 16:36:49 +1100

    Initial Commit

---
+++ Investment Alligator/Investment_AlligatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Investment_AlligatorApp.swift
+//  Investment Alligator
+//
+//  Created by John Reid on 13/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct Investment_AlligatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
